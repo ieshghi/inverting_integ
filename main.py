c181de2ab1b9d7ef2b8f938c34a042f3bcf943ba
Iraj Eshghi <irajeshghi@gmail.com>
2018-11-06 00:59:50 -0500

    issues with normalisation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from numpy import *
 import matplotlib.pyplot as plt
 from scipy.integrate import RK45
+from numpy.random import randn
+import numpy as np
+
 def int_weights(n,dx = 1,method = 0):
     if method == 0: #trapezoidal rule
         a = dx*ones(n)
@@ -22,16 +25,18 @@ def gen_mat(n,ts,l): #makes nxn matrix of discretised integral
     mat[0,0] = dt
     for i in range(1,n):
         a = int_weights(i+1,dt)
-        for j in range(i+1):
-            mat[i,j] = a[j]*exp(-l*abs(ts[i]-ts[j]))
-    return mat
+        mat[i,0:(i+1)] = a[:(i+1)]*exp(-l*abs(ts[i]-ts[:(i+1)]))
+    return mat*n
 
 def findv(rhs,t_i,t_f,l):
     n = rhs.size
     ts = linspace(t_i,t_f,n)
     m = gen_mat(n,ts,l)
     v = linalg.solve(m,rhs)
-    return v
+    signswitch = ones(v.size)
+    signswitch[1::2] = -1
+
+    return v*signswitch
 
 def find_r(r_i,v,t_i,t_f):
     dt = (t_f-t_i)/v.size
@@ -41,13 +46,56 @@ def find_r(r_i,v,t_i,t_f):
         r[i] = r[i-1] + v[i-1]*dt
     return r
 
+def corrnoise(n,l):
+    whitenoise = sqrt(2)*randn(n)
+    nums = zeros(n)
+    nums[0] = whitenoise[0]
+    b = l
+    
+    for i in range(1,n):
+        a = -l*nums[i-1]
+        nums[i] = nums[i-1] + a + b*whitenoise[i]
+    
+    nums_out = nums/sqrt(l)
+    return nums_out
+
 def simulate_maxwell(n):
     t0 = 0
     t1 = 1
-    l = 10
-    noise = zeros(n)
-    for i in range(n):
-        noise[i]= sqrt(2)*random.normal()
-    v = findv(noise,t0,t1,l)
-    r = find_r(0,v,0,1)
-    return r
+    l = 2.
+    noise_x = corrnoise(n,l)
+    noise_y = corrnoise(n,l)
+    vx = findv(noise_x,t0,t1,l)
+    vy = findv(noise_y,t0,t1,l)
+    rx = find_r(0,vx,0,1)
+    ry = find_r(0,vy,0,1)
+    return rx,ry
+
+def msd(rx,ry):
+    r = sqrt(rx**2+ry**2)
+    diffr = diff(r) #this calculates r(t + dt) - r(t)
+    diff_sq = diffr**2
+    MSD = mean(diff_sq)
+    return MSD
+
+def autocorrFFT(x):
+  N=len(x)
+  F = np.fft.fft(x, n=2*N)  #2*N because of zero-padding
+  PSD = F * F.conjugate()
+  res = np.fft.ifft(PSD)
+  res= (res[:N]).real   #now we have the autocorrelation in convention B
+  n=N*np.ones(N)-np.arange(0,N) #divide res(m) by (N-m)
+  return res/n #this is the autocorrelation in convention A
+
+def msd_fft(r):
+  N=len(r)
+  D=np.square(r).sum(axis=1)
+  D=np.append(D,0)
+  S2=sum([autocorrFFT(r[:, i]) for i in range(r.shape[1])])
+  Q=2*D.sum()
+  S1=np.zeros(N)
+  for m in range(N):
+      Q=Q-D[m-1]-D[N-m]
+      S1[m]=Q/(N-m)
+  return S1-2*S2
+
