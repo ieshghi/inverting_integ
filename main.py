eeb7089b37b78fdf9ddc56b14be641251a94cb6b
Iraj Eshghi <irajeshghi@gmail.com>
2018-11-20 00:04:17 -0500

    simulation works, but boundary conditions are janky and output is weirdly simple...

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ def int_weights(n,dx = 1,method = 0):
         a = dx*ones(n)
         a[0] = dx/2
         a[-1] = dx/2
+        a = a
     elif method == 1: 
-        a = dx*ones(n)*1./3
+        a = dx*ones(n)
         a[1::2] = dx*4./3
         a[0::2] = dx*2./3
         a[0] = dx/3
@@ -32,11 +33,10 @@ def findv(rhs,t_i,t_f,l):
     n = rhs.size
     ts = linspace(t_i,t_f,n)
     m = gen_mat(n,ts,l)
-    v = linalg.solve(m,rhs)
-    signswitch = ones(v.size)
-    signswitch[1::2] = -1
+    mi = abs(linalg.inv(m))
+    v = mi.dot(rhs)
 
-    return v*signswitch
+    return v
 
 def find_r(r_i,v,t_i,t_f):
     dt = (t_f-t_i)/v.size
@@ -55,10 +55,9 @@ def corrnoise(n,l):
 
     return nums_out
 
-def simulate_maxwell(n):
+def simulate_maxwell(n,l):
     t0 = 0
     t1 = 1
-    l = 2.
     noise_x = corrnoise(n,l)
     noise_y = corrnoise(n,l)
     vx = findv(noise_x,t0,t1,l)
@@ -67,31 +66,14 @@ def simulate_maxwell(n):
     ry = find_r(0,vy,0,1)
     return rx,ry
 
-def msd(rx,ry):
-    r = sqrt(rx**2+ry**2)
-    diffr = diff(r) #this calculates r(t + dt) - r(t)
-    diff_sq = diffr**2
-    MSD = mean(diff_sq)
-    return MSD
-
-def autocorrFFT(x):
-  N=len(x)
-  F = np.fft.fft(x, n=2*N)  #2*N because of zero-padding
-  PSD = F * F.conjugate()
-  res = np.fft.ifft(PSD)
-  res= (res[:N]).real   #now we have the autocorrelation in convention B
-  n=N*np.ones(N)-np.arange(0,N) #divide res(m) by (N-m)
-  return res/n #this is the autocorrelation in convention A
-
-def msd_fft(r):
-  N=len(r)
-  D=np.square(r).sum(axis=1)
-  D=np.append(D,0)
-  S2=sum([autocorrFFT(r[:, i]) for i in range(r.shape[1])])
-  Q=2*D.sum()
-  S1=np.zeros(N)
-  for m in range(N):
-      Q=Q-D[m-1]-D[N-m]
-      S1[m]=Q/(N-m)
-  return S1-2*S2
-
+def msd(trajx,trajy):
+    traj = vstack((trajx,trajy))
+    n = trajx.size
+    shifts = linspace(1,n-1,n-1)
+    msds = np.zeros(shifts.size)
+    for i, shift in enumerate(shifts):
+        shift = int(shift)
+        diffs = traj - roll(traj,-shift)
+        sqdist = np.square(diffs).sum(axis=1)
+        msds[i] = sqdist.mean()
+    return msds
